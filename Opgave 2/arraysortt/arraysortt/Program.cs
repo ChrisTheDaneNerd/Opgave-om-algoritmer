667c189b895562d259d0f095e63a36a401dfa583
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-10-24 11:05:34 +0200

    opgave 2 done and over
    :D

---
+++ Opgave 2/arraysortt/arraysortt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraysortt
+{
+    internal class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 8, 6, 2, 3, 9, 7, 5 };
+            InsertionSort ob = new InsertionSort();
+            ob.sort(arr);
+            printarray(arr);
+            Console.WriteLine(arr[0]);
+            Console.ReadKey();
+
+
+
+
+        }
+
+        void sort(int[] arr)
+        {
+            int n = arr.Length;
+            for(int i = 0; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while(j >= 0&& arr[j]<key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j+1] = key;
+            }
+        }
+        static void printarray(int[] arr)
+        {
+            int n = arr.Length;
+            for(int i = 0; i < n;i++)
+            {
+                Console.Write(arr[i]+ " ");
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
