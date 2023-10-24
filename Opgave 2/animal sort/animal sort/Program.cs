139be6d3b7a0b53b5cd471d95a2414141b8dd9e7
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-10-24 12:13:17 +0200

    WIP opgave 3
    opgave 3

---
+++ Opgave 2/animal sort/animal sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = new string[28];
+            string path = @".\arter.txt";
+            try
+            {
+                //FileInfo fi = new FileInfo(path);
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+
+                string line = "";
+                int i = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    animals[i] = line;
+                    i++;
+                }
+                fs.Close();
+                sr.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File doesn't exist, create file");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
